f308afeae4e6b8d9f56eb22328d2ca1ceb9d5238
Bruno Rodrigues <bruno.brds@hotmail.com>
2021-04-30 14:42:27 -0300

    feat: clear trash and create folders structures

---
+++ src/pages/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Normalize } from 'styled-normalize';
+import Home from './Home';
+
+function App() {
+  return (
+    <>
+      <Normalize />
+      <Home/>
+    </>
+  );
+}
+
+export default App;
