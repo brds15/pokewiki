f0e8f06a5cbef148259cda9eea2e32f36395d33e
Bruno Rodrigues <bruno.brds@hotmail.com>
2021-05-02 14:23:03 -0300

    feat: create api request file

---
+++ src/services/api/pokeapi.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import i18n from 'i18next';
+const API_BASE = 'https://pokeapi.co/api/v2/';
+
+export const getPokes = async () => {
+  try {
+    const { data } = await axios.get(`${API_BASE}generation`);
+    return data;
+  } catch (error) {
+    alert(i18n.t('generalApiError'));
+    console.error(error);
+  }
+};
