6efbce59bc1cdc99d27ddd5867548924f2324c30
Bruno Rodrigues <bruno.brds@hotmail.com>
2021-05-02 18:53:10 -0300

    feat: change colors schema

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -5,7 +5,7 @@ const colors = {
   gray: '#7f7f8b',
   gray2: '#3a3a44',
   blue: '#054f77',
-  blue2: '#2b96e8',
+  blue2: '#0f6a9e',
   yellow: '#f5ef22',
   orange: '#f52231',
 };
@@ -16,8 +16,8 @@ export const dark = {
   white: '#000000',
   gray: '#7f7f8b',
   gray2: '#cec8c8',
-  blue: '#054f77',
-  blue2: '#2b96e8',
+  blue: '#0f6a9e',
+  blue2: '#0f6a9e',
   yellow: '#f5ef22',
   orange: '#f52231',
 };
