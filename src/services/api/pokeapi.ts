942164e9f8bef5936ea5c806c8571fc8c36932d5
Bruno Rodrigues <bruno.brds@hotmail.com>
2021-05-02 22:22:12 -0300

    feat: fetch pokemons by generations

--- src/services/api/pokeapi.ts
+++ src/services/api/pokeapi.ts
@@ -12,3 +12,13 @@ export const getGenerations = async () => {
     console.error(error);
   }
 };
+
+export const getGeneration = async (generationNumber: string) => {
+  try {
+    const { data } = await axios.get(`${API_BASE}generation/${generationNumber}/`);
+    return data;
+  } catch (error) {
+    alert(i18n.t('generalApiError'));
+    console.error(error);
+  }
+};
