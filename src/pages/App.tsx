b564f501cbc8aa5a46fe0c21c49ebcc59d220b1d
Bruno Rodrigues <bruno.brds@hotmail.com>
2021-05-04 23:39:49 -0300

    feat: add detail context to app container

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -4,19 +4,22 @@ import Home from './Home';
 import { StyleContainer, Content, Wrapper } from '../styles/globals';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import DetailProvider from '../context/DetailProvider';
 
 function App() {
   return (
     <>
       <Normalize />
       <StyleContainer>
-        <Header />
-        <Content>
-          <Wrapper>
-            <Home />
-          </Wrapper>
-        </Content>
-        <Footer />
+        <DetailProvider>
+          <Header />
+          <Content>
+            <Wrapper>
+              <Home />
+            </Wrapper>
+          </Content>
+          <Footer />
+        </DetailProvider>
       </StyleContainer>
     </>
   );
